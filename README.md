b3cc8db123946c051919221070b07cf621d9d734
prokopparuzek <prokop.paruzek@paruzkovi.cz>
2018-12-02 12:43:25 +0100

    make function

--- README.md
+++ README.md
@@ -3,6 +3,10 @@ Aplikace je napsaná v Pythonu3. Použití je.
 ```
 ./reg.py <název vstupního souboru> <název výstupního souboru> <jméno turnaje>
 ```
-Nezapomeňte na udělení práv ke spuštění souboru reg.py.  
-Vstupní soubor musí být v kódování utf-8.  
-Z výstupního souboru odstraňuje diakritiku.  
+## Závislosti
+Modul: `unidecode`  
+Odkaz na domovskou stránku modulu: [https://pypi.org/project/Unidecode/](https://pypi.org/project/Unidecode/).
+
+## Možné problémy
+1. Soubor `reg.py` nemá práva na spuštění.
+2. Vstupní soubor není v kódování utf-8.
--- reg.py
+++ reg.py
@@ -3,6 +3,27 @@
 import re,sys
 from unidecode import unidecode
 
+def uprav(csv, turnaj):
+    csv = re.sub(r'([A-Z]{2,3})\n', r'\1', csv)
+    csv = csv.split('\n')
+    patS = r'^(Male|Female)'
+    patC = r'Category:;*([^/]*)'
+    patM = r'^;\d'
+    fs = ';'
+    out = []
+    for line in csv:
+        if re.search(patS, line):
+            sex = re.search(patS, line).group(0)
+        elif re.search(patC, line):
+            cat = re.search(patC, line).group(1).rstrip()
+        elif re.match(patM, line):
+            mem = re.search(r'^;*([0-9]+).*\(([0-9]+)\);*([^;]+);*([A-Z]+);+([^;]+);*$', line)
+            data = []
+            for i in range(1,6):
+                data.append(mem.group(i))
+            out.append(cat + fs + sex + fs + price(data[0]) + fs + Wprice(data[1]) + fs + data[2] + fs + data[3] + fs + unidecode(data[4]) + fs + turnaj + fs + Aprice(data[0:2]))
+    return out
+
 def price(mista):
     bod = 0
     if mista == '1':
@@ -24,27 +45,8 @@ def Aprice(data):
 coding = "utf-8"
 with open(sys.argv[1], mode="r", encoding=coding) as i:
     csv = i.read()
-    csv = re.sub(r'([A-Z]{2,3})\n', r'\1', csv)
-    csv = csv.split('\n')
-    patS = r'^(Male|Female)'
-    patC = r'Category:;*([^/]*)'
-    patM = r'^;\d'
-    fs = ';'
-    out = []
-    for line in csv:
-        if re.search(patS, line):
-            sex = re.search(patS, line).group(0)
-            #print(sex)
-        elif re.search(patC, line):
-            cat = re.search(patC, line).group(1).rstrip()
-            #print(cat)
-        elif re.match(patM, line):
-            mem = re.search(r'^;*([0-9]+).*\(([0-9]+)\);*([^;]+);*([A-Z]+);+([^;]+);*$', line)
-            data = []
-            for i in range(1,6):
-                data.append(mem.group(i))
-            out.append(cat + fs + sex + fs + price(data[0]) + fs + Wprice(data[1]) + fs + data[2] + fs + data[3] + fs + unidecode(data[4]) + fs + sys.argv[3] + fs + Aprice(data[0:2]))
-            #print(out)
+    out = uprav(csv, sys.argv[3])
 with open(sys.argv[2], mode="w", encoding=coding) as o:
     for line in out:
         o.write(line + '\n')
+
