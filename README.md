2dbc34b5922778d869c14268d1412b1ccd52fa6a
prokopparuzek <prokop.paruzek@paruzkovi.cz>
2018-12-02 09:06:28 +0100

    Remove diacritic

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ Aplikace je napsaná v Pythonu3. Použití je.
 ./reg.py <název vstupního souboru> <název výstupního souboru> <jméno turnaje>
 ```
 Nezapomeňte na udělení práv ke spuštění souboru reg.py.
+Vstupní soubor musí být v kódování utf-8.
+Z výstupního souboru odstraňuje diakritiku.
--- reg.py
+++ reg.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 import re,sys
+from unidecode import unidecode
 
 def price(mista):
     bod = 0
@@ -20,20 +21,17 @@ def Aprice(data):
     bod = int(price(data[0])) + int(Wprice(data[1]))
     return str(bod)
 
-coding = "latin2"
+coding = "utf-8"
 with open(sys.argv[1], mode="r", encoding=coding) as i:
     csv = i.read()
     csv = re.sub(r'([A-Z]{2,3})\n', r'\1', csv)
-    csv = csv.rstrip()
-with open(sys.argv[1], mode="w", encoding=coding) as i:
-    i.write(csv)
-with open(sys.argv[1], mode="r", encoding=coding) as i:
+    csv = csv.split('\n')
     patS = r'^(Male|Female)'
     patC = r'Category:;*([^/]*)'
     patM = r'^;\d'
     fs = ';'
     out = []
-    for line in i:
+    for line in csv:
         if re.search(patS, line):
             sex = re.search(patS, line).group(0)
             #print(sex)
@@ -45,7 +43,7 @@ with open(sys.argv[1], mode="r", encoding=coding) as i:
             data = []
             for i in range(1,6):
                 data.append(mem.group(i))
-            out.append(cat + fs + sex + fs + price(data[0]) + fs + Wprice(data[1]) + fs + data[2] + fs + data[3] + fs + data[4] + fs + sys.argv[3] + fs + Aprice(data[0:2]))
+            out.append(cat + fs + sex + fs + price(data[0]) + fs + Wprice(data[1]) + fs + data[2] + fs + data[3] + fs + unidecode(data[4]) + fs + sys.argv[3] + fs + Aprice(data[0:2]))
             #print(out)
 with open(sys.argv[2], mode="w", encoding=coding) as o:
     for line in out:
